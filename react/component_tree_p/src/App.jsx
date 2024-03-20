9a1b676665010d407f139fa2ecde57a1c6c659d6
AkritiSubedi <aakusubedi075@gmail.com>
2024-03-20 18:12:11 +0545

    few changes

--- react/component_tree_p/src/App.jsx
+++ react/component_tree_p/src/App.jsx
@@ -24,7 +24,7 @@ function App() {
   return (
     <div className="container">
       <div className="heading">
-        <h1>To-Do List</h1>
+        <h1>To-Do-List</h1>;
       </div>
       <InputArea 
       onAdd={addItem}
